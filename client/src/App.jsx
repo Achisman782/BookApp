e689edb6c880debc821fa9e69705b01742714c77
Achisman782 <achismanbanerjee4@gmail.com>
2024-03-21 16:19:47 +0530

    More Changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import LoginPage from './components/LoginPage'
 import Layout from './Layout'
 import RegisterPage from './components/RegisterPage'
 import axios from 'axios'
+import { UserContextProvider } from './UserContext'
 
 axios.defaults.baseURL='http://127.0.0.1:4321'
 axios.defaults.withCredentials = true
@@ -13,13 +14,15 @@ function App() {
 
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-      <Route index element={<IndexPage />} />
-      <Route path="/login" element={<LoginPage />}/ >
-      <Route path='/register' element={<RegisterPage />} />
-      </Route> 
-    </Routes>
+    <UserContextProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />} />
+          <Route path="/login" element={<LoginPage />}/ >
+          <Route path='/register' element={<RegisterPage />} />
+        </Route> 
+      </Routes>
+    </UserContextProvider>
     </>
   )
 }
--- client/src/components/LoginPage.jsx
+++ client/src/components/LoginPage.jsx
@@ -1,18 +1,22 @@
 import axios from "axios";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Link, Navigate } from "react-router-dom";
+import { UserContext } from "../UserContext";
 
 export default function LoginPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [redirect, setRedirect] = useState('');
+    const {setUser} = useContext(UserContext);
+
     async function handleLoginSubmit(ev) {
         ev.preventDefault();
         try {
-            await axios.post("/login", {email, password})
+            const {data} = await axios.post("/login", {email, password})
+            setUser(data);
             alert("Login successful!");
             setRedirect(true);
-        }   catch (e) {
+        }   catch (err) {
             alert("Login failed!"); 
         }
     }
